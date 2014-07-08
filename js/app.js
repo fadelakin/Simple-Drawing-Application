95a477b8ff0c07a4b2f3dfe66eee24ee373d4783
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-07-08 01:48:36 -0400

    color selector works

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ $(".controls li").click(function() {
 	// Cache current color
 	color = $(this).css("background-color");
 });
+
+// When new color is pressed
+$("#revealColorSelect").click(function() {
+	// Show color selection or hide the color selection
+	changeColor();
+	$("#colorSelect").toggle();
+});
+
+// Update the new color span
+function changeColor() {
+	var r = $("#red").val();
+	var g = $("#green").val();
+	var b = $("#blue").val();
+	$("#newColor").css("background-color", "rgb(" + r + "," + g + "," + b + ")");
+}
+
+// When color sliders change
+$("input[type=range]").change(changeColor);
